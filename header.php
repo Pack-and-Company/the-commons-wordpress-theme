44b438c54a3d8d02ac7ac800bc883e56332c6cb4
Tom Henderson <tomhenderson@mac.com>
2014-08-25 10:21:07 +1200

    Add css reset and style.css.

--- header.php
+++ header.php
@@ -10,7 +10,17 @@
       bloginfo( 'name' );
     ?>
   </title>
+
+  <link href='http://fonts.googleapis.com/css?family=Lato:400,900,100italic' rel='stylesheet' type='text/css'>
+  
+  <link href="<?=get_template_directory_uri();?>/css/reset.css" rel="stylesheet" media="screen" type="text/css"/>
+  <link href="<?=get_template_directory_uri();?>/css/prettyPhoto.css" rel="stylesheet" media="screen" type="text/css"/>
+  <link href="<?=get_template_directory_uri();?>/css/style.css" rel="stylesheet" media="screen" type="text/css"/>
+
   <?php wp_head(); ?>
+
+  <script src="<?=get_template_directory_uri();?>/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+  <script src="<?=get_template_directory_uri();?>/js/scripts.js" type="text/javascript" charset="utf-8"></script>
 </head>
 
 <body <?php body_class(); ?>>
