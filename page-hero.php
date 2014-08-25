92a9e646987d5bb68f64b4f66ff044b1324d1128
Tom Henderson <tomhenderson@mac.com>
2014-08-25 13:36:51 +1200

    Remove extra closing tag.

--- page-hero.php
+++ page-hero.php
@@ -7,7 +7,7 @@ Template Name: Hero Image
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<h2><?php the_title(); ?></h2>h2>
+	<h2><?php the_title(); ?></h2>
 <div class="hero-image">
 	<?php
 	if ( has_post_thumbnail() ) {
