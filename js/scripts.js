fee0031beaa95364f18299d34dc9f833826b67e5
Tom Henderson <tomhenderson@mac.com>
2014-08-26 10:00:06 +1200

    Initialize prettyPhoto.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function($){
-	// Scripts
+
+	// Generic function to initialize prettyPhoto
+	function init_prettyPhoto(target) {
+		var gallery_name = 'prettyPhoto[' + target + ']';
+
+		$(target + ' a').attr('rel', gallery_name);
+		$("a[rel^='" + gallery_name + "']").prettyPhoto();
+		$(target + ":first a[rel^='" + gallery_name + "']").prettyPhoto({
+			animation_speed:'normal',
+			slideshow:8000,
+			social_tools:false,
+			autoplay_slideshow: false,
+			overlay_gallery:false,
+			show_title: true,
+			allow_resize: true,
+			default_width: 320,
+			wmode: 'opaque'
+		});
+	}
+
+	init_prettyPhoto('.gallery');
+	
 }
