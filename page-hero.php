eac2632e749c3d07eef591d158a35bc5b258ac09
Tom Henderson <tomhenderson@mac.com>
2014-08-25 14:26:34 +1200

    Use h1 for page title.

--- page-hero.php
+++ page-hero.php
@@ -7,17 +7,17 @@ Template Name: Hero Image
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<h2><?php the_title(); ?></h2>
-<div class="hero-image">
-	<?php
-	if ( has_post_thumbnail() ) {
-		the_post_thumbnail('full');
-	}
-	?>
-	<div class="hero-text">
-	  <?php the_content(); ?>
-  	</div>
-</div>
+	<h1><?php the_title(); ?></h1>
+	<div class="hero-image">
+		<?php
+		if ( has_post_thumbnail() ) {
+			the_post_thumbnail('full');
+		}
+		?>
+		<div class="hero-text">
+		  <?php the_content(); ?>
+	  	</div>
+	</div>
 <?php endwhile; wp_reset_query(); ?>
 
 <?php get_footer(); ?>
