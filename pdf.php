97fdf567f8e312a0352d1c40858cae35e204dd3e
Tom Henderson <tomhenderson@mac.com>
2014-08-25 15:33:10 +1200

    Move pdf iframe styling to css.

--- pdf.php
+++ pdf.php
@@ -3,7 +3,7 @@
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
   <h1><?php the_title(); ?></h1>
 
-  <iframe src="http://docs.google.com/gview?url=<?= wp_get_attachment_url( $post->id ); ?>&embedded=true" style="width:960px; height:500px;" frameborder="0"></iframe>
+  <iframe src="http://docs.google.com/gview?url=<?= wp_get_attachment_url( $post->id ); ?>&embedded=true" class="pdf" frameborder="0"></iframe>
 
 <?php endwhile; wp_reset_query(); ?>
 
